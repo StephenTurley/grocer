c9888ef1455fc9958c58a2769c5c30d33a346aff
Stephen Turley <turley714@gmail.com>
2017-09-24 12:45:30 -0400

    added build.sh for ci. waits for neo to start

---
+++ build.sh
@@ -0,0 +1,15 @@
+function wait_for_neo {
+  status=0
+  while [ $status -ne 200 ]
+  do
+    status="$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://localhost:7575/)"
+    echo "Wating for Neo4j to respond, current http status is $status"
+    sleep 1
+  done
+}
+
+
+docker-compose up -d
+wait_for_neo
+RAILS_ENV=test bundle exec rake neo4j:migrate
+bundle exec rake spec
